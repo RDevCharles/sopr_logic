9adaa1f542de0288a12bc2334cb80fbbe93fd074
RDevCharles <rdevcharles@protonmail.com>
2021-11-29 00:36:40 -0500

    beta ready

--- token_scraper.py
+++ token_scraper.py
@@ -12,40 +12,58 @@ cg = CoinGeckoAPI()
 INFURA_APIKEY = os.getenv("INFURA_API_KEY")
 ETHERSCAN_APIKEY = os.getenv("ETHERSCAN_API_KEY")
 
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_SECRET_KEY")
+supabase: Client = create_client(url, key)
+
 
 #shitcoin list
 shitcoin_list = ['floki-inu', 'shiba-inu', 'saitama-inu']
+listdata = supabase.table('coin_list').select('name').execute()
+
+print(listdata['data'])
+
+for h in listdata['data']:
+    for j in h:
+        print(h[j])
+
+
 
 #all the data being sent to database
 
 #while True:
 def main():
-    for i in shitcoin_list:
-        gas_price_req = requests.get(
-            f'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={ETHERSCAN_APIKEY}')
-        gas_price_res = gas_price_req.json()['result']['SafeGasPrice']
-        coin_address_req = cg.get_coin_by_id(i)
-        coin_address_res = coin_address_req['platforms']['ethereum']
-        coin_price_req = cg.get_price(
-            ids=i, vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true')
-      
-        total_supply_req = requests.get(
-            f'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress={coin_address_res}&apikey={ETHERSCAN_APIKEY}')
-        total_supply_res = total_supply_req.json()['result']
+    for h in listdata['data']:
+        for j in h:
+            gas_price_req = requests.get(
+                f'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey={ETHERSCAN_APIKEY}')
+            gas_price_res = gas_price_req.json()['result']['SafeGasPrice']
+            coin_address_req = cg.get_coin_by_id(h[j])
+            coin_address_res = coin_address_req['platforms']['ethereum']
+            coin_price_req = cg.get_price(
+                ids=h[j], vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true')
+        
+            total_supply_req = requests.get(
+                f'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress={coin_address_res}&apikey={ETHERSCAN_APIKEY}')
+            total_supply_res = total_supply_req.json()['result']
 
-        if 0 > coin_price_req[i]['usd_24h_volume'] / coin_price_req[i]['usd_24h_change']:
-            score_color = 'red'
-        elif 0.5 < coin_price_req[i]['usd_24h_volume'] / coin_price_req[i]['usd_24h_change']:
-            score_color = 'green'
-        else:
-            score_color = 'yellow'
-            score_color = 'yellow'
-        ct = str(datetime.datetime.now())
-        url: str = os.getenv("SUPABASE_URL")
-        key: str = os.getenv("SUPABASE_SECRET_KEY")
-        supabase: Client = create_client(url, key)
-        data = supabase.table("alts").insert({"name":i, "address" :coin_address_res, "info" : coin_price_req[i], "supply":total_supply_res, "gas" : gas_price_res, "timestamp":ct, "color":score_color }).execute()
-        assert len(data.get("data", [])) > 0
+            if 0 > coin_price_req[h[j]]['usd_24h_vol'] / coin_price_req[h[j]]['usd_24h_change']:
+                score_color = 'red'
+            elif 0.5 < coin_price_req[h[j]]['usd_24h_vol'] / coin_price_req[h[j]]['usd_24h_change']:
+                score_color = 'green'
+            else:
+                score_color = 'yellow'
+                score_color = 'yellow'
+            ct = str(datetime.datetime.now())
+            
+            data = supabase.table("alts").insert({"name":h[j], "address" :coin_address_res, "info" : coin_price_req[h[j]], "supply":total_supply_res, "gas" : gas_price_res, "timestamp":ct, "color":score_color }).execute()
+            assert len(data.get("data", [])) > 0
+
+
+        
+
+       
+
       
 
 main()
