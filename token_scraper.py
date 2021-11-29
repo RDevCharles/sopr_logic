ffd8a01fc44a930f7a45b2bf8f50ee36f9c5e95e
RDevCharles <rdevcharles@protonmail.com>
2021-11-29 13:33:16 -0500

    code clean up

--- token_scraper.py
+++ token_scraper.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from supabase_py import create_client, Client
 from pycoingecko import CoinGeckoAPI
 import requests
-import datetime;
+import datetime
+import time
 
 load_dotenv()
 
@@ -23,8 +24,8 @@ listdata = supabase.table('coin_list').select('name').execute()
 
 #SUPABASE FUNCTIONS
 
-#while True:
-def main():
+while True:
+
     for h in listdata['data']:
         for j in h:
             gas_price_req = requests.get(
@@ -34,7 +35,7 @@ def main():
             coin_address_res = coin_address_req['platforms']['ethereum']
             coin_price_req = cg.get_price(
                 ids=h[j], vs_currencies='usd', include_market_cap='true', include_24hr_vol='true', include_24hr_change='true')
-        
+
             total_supply_req = requests.get(
                 f'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress={coin_address_res}&apikey={ETHERSCAN_APIKEY}')
             total_supply_res = total_supply_req.json()['result']
@@ -48,18 +49,10 @@ def main():
                 score_color = 'yellow'
                 score_color = 'yellow'
             ct = str(datetime.datetime.now())
-            
+
             #SUPABASE UPLOAD
-            data = supabase.table("alts").insert({"name":h[j], "address" :coin_address_res, "info" : coin_price_req[h[j]], "supply":total_supply_res, "gas" : gas_price_res, "timestamp":ct, "color":score_color }).execute()
+            data = supabase.table("alts").insert({"name": h[j], "address": coin_address_res, "info": coin_price_req[h[j]],
+                                                  "supply": total_supply_res, "gas": gas_price_res, "timestamp": ct, "color": score_color}).execute()
             assert len(data.get("data", [])) > 0
 
-
-        
-
-       
-
-      
-
-main()
-#time.sleep(3)
-
+    time.sleep(30)
